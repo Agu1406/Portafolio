ec5e4e1637f88f554021636a8e99841300b29f0c
Agu1406 <agustin6041@outlook.es>
2024-04-01 05:15:46 +0200

    Quinto ejercício de la Unidad Teórica 2, consistía en el análisis de un código ya escrito en el que se nos introducía a más ejemplos de operadores aritméticos y además a las diferentes formar de imprimir y formatear texto en pantalla (println y printf)

---
+++ src/UT2JavaOperadoresYVariables/MPAAEjercicio5.java
@@ -0,0 +1,18 @@
+package UT2JavaOperadoresYVariables;
+
+/**
+ * Ejercicio N.º5 - Operadores aritméticos, usando println y printf. En el ejemplo vemos un
+ * programa básico que utiliza operadores aritméticos. Observa que usamos tanto
+ * System.out.println como System.out.printf para mostrar por pantalla un texto
+ * formateado. Primero indicamos cómo queremos que salga el texto, y después
+ * las variables que se mostrarán con el texto. Con %f nos referimos a una
+ * variable de tipo float, con %d un entero en base decimal, etc.
+ *
+ * @author Agu1406 (Agustín)
+ * @since 06/10/2023
+ */
+public class MPAAEjercicio5 {
+    public static void main(String[] args) {
+
+    }
+}
