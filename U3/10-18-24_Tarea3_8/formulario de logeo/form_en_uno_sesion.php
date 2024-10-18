f9ac37ec4c6c247a5c99591b7855a49c191e32a1
agu1406 <agustin6041@outlook.es>
2024-10-18 14:11:49 +0200

    Ningún cambio importante, a penas he creado un "if" en la logica de inicio de sesión que verifica si el método de envio del PHP es POST o no.

--- U3/10-18-24_Tarea3_8/formulario de logeo/form_en_uno_sesion.php
+++ U3/10-18-24_Tarea3_8/formulario de logeo/form_en_uno_sesion.php
@@ -5,3 +5,7 @@
  * de sesión coherente, que valide los datos introducidos y dependiendo de una cosa
  * u otra permita cerrar sesión o entrar al sitio web una vez validados los datos.
  */
+
+ // Si el método de envio del formulario es POST, se ejecuta el "if", si no, no.
+if ($_SERVER["REQUEST_METHOD"]== "POST") {
+}
