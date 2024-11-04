b2e5415d1fb29688561b3cef070eac5b795541f6
Agu1406 <agustin6041@outlook.es>
2024-11-04 08:56:57 +0100

    Finalización del ejercicio 3_10 conectando una base de datos en localhost con PHP y realizando consultas inmediatas o preparadas con execute donde filtramos, por ejemplo, usuarios de diferentes roles.

--- U3/10-23-24_Tarea3_10ConectarBD/bd_recuperar_y_presentar_datos.php
+++ U3/10-23-24_Tarea3_10ConectarBD/bd_recuperar_y_presentar_datos.php
@@ -39,9 +39,65 @@ try {
     $usuarios = $baseDeDatos -> query($consulta);
 
     /**
-     * 
+     * De la lista de usuarios obtenida de la consulta realizada
+     * con un bucle foreach recorre una a una las posiciones del
+     * array e imprime de cada una de las instancias de "usarios"
+     * los datos que deseamos, en este caso "nombre", "clave" y
+     * "rol".
      * */
-    foreach ($usuarios as $espejo) {}
-} catch (PDOException $e) {
+    foreach ($usuarios as $espejo) {
+        print "Nombre del usuario: " . espejo["nombre"] . " | ";
+        print "Clave / Contraseña: " . espejo["clave"] . " | ";
+        print "Rol del usuario: " . espejo["rol"] . "<br>";
+    }
 
+    /**
+     * Creamos una consulta pre "preparada" para filtrar a todos los usuarios cuyas
+     * roles de usuario sean "0".
+     */
+    $preparada = $baseDeDatos -> prepare("SELECT nombre, clave, rol FROM usuarios WHERE rol =:rol");
+    $preparada -> execute(array(":rol" => 0));
+
+    // Salto de linea, avisamos lo que vamos a imprimir y otro salto de linea.
+    echo "<br>Los usuarios con rol \"0\" son los siguientes: <br>";
+
+    /**
+     * Con un bucle foreach recorremos el array "preparada" donde hemos ejecutado
+     * y guardado en forma de array los resultados de la consulta donde hemos
+     * filtrado que se vean los usuarios de rol "0" solamente.
+     */
+    foreach ($preparada as $usuario) {
+        print "Nombre del usuario: " . usuario["nombre"] . " | ";
+        print "Clave / Contraseña: " . usuario["clave"] . " | ";
+        print "Rol del usuario: " . usuario["rol"] . "<br>";
+    }
+
+    /**
+     * Creamos una consulta pre "preparada" para filtrar a todos los usuarios cuyas
+     * roles de usuario sean "0".
+     */
+    $preparada2 = $baseDeDatos -> prepare("SELECT nombre, clave, rol FROM usuarios WHERE rol =:rol");
+    $preparada2 -> execute(array(":rol" => 2222));
+
+    // Salto de linea, avisamos lo que vamos a imprimir y otro salto de linea.
+    echo "<br>Los usuarios con rol \"2222\" son los siguientes: <br>";
+
+    /**
+     * Con un bucle foreach recorremos el array "preparada" donde hemos ejecutado
+     * y guardado en forma de array los resultados de la consulta donde hemos
+     * filtrado que se vean los usuarios de rol "0" solamente.
+     */
+    foreach ($preparada2 as $usuario) {
+        print "Nombre del usuario: " . usuario["nombre"] . " | ";
+        print "Clave / Contraseña: " . usuario["clave"] . " | ";
+        print "Rol del usuario: " . usuario["rol"] . "<br>";
+    }
+
+} catch (PDOException $e) {
+    /**
+     * De entrada si la conexión con la base de datos no fue exitosa imprimimos
+     * un mensaje general personalizado de error y luego imprimimos el mensaje
+     * especifico captura por la excepción en la variable "$e".
+     */
+    echo "¡Error! No se ha podido establecer conexión con la base de datos: <br>" . $e -> getMessage();
 }
