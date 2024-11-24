fc405711e6f8053ea80c7fdc4ddbb09d5bf51880
Agu1406 <Agustin6041@outlook.com>
2024-11-24 21:29:32 +0100

    Primeros tres ejercicios del Topic 1 de JavaScript donde aprendimos conceptos basicos del lenguaje como concatenar o sumar strings o valores así como usar scripts internos, externos, los tipos de variables y el uso de "alert" y "prompt".

---
+++ Portafolio/DAW2/RepasoExamenes/Repaso Desarrollo Web Entorno Cliente/Topic_1/scripts/T1_2_Script.js
@@ -0,0 +1,12 @@
+/**
+ * Utiliza el documento de la tarea E1-1.1, mueve el código JavaScript a un 
+ * archivo llamado «script.js» y enlázalo con el html.
+ * 
+ * Comprueba que funciona, enséñaselo al docente y entrega aquí ambos archivos 
+ * en un comprimido «E1-1.2.zip».
+ */
+
+// Creamos un alert que imprime un mensaje en ventana emergente.
+alert("¡Bienvenid@ a 2º DAW");
+// Enviamos por consola el nombre del centro educativo.
+console.log("IES Arquitecto Ventura Rodriguez");
