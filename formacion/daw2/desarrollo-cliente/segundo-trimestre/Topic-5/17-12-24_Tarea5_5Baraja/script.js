c7def9bead656b9a558f71b6f5ee12dcb06bffcd
Agu1406 <agustin6041@outlook.com>
2024-12-17 18:28:25 +0100

    feat(entorno-cliente): Creación de hoja de estilos más limpia.

--- formacion/daw2/desarrollo-cliente/segundo-trimestre/Topic-5/17-12-24_Tarea5_5Baraja/script.js
+++ formacion/daw2/desarrollo-cliente/segundo-trimestre/Topic-5/17-12-24_Tarea5_5Baraja/script.js
@@ -2,3 +2,28 @@
  * No usar la variable "var", está en deshuso y no es profesional, usar
  * let o const en su lugar.
  */
+
+/**
+ * Clase Carta
+ * Representa una carta de una baraja española con dos propiedades:
+ * 
+ * palo: un número del 1 al 4 que representa el palo de la carta:
+ *       1 - Oros
+ *       2 - Espadas
+ *       3 - Bastos
+ *       4 - Copas
+ * 
+ * valor: un número del 1 al 10 que representa el valor de la carta:
+ *        1 - As
+ *        8 - Sota
+ *        9 - Caballo
+ *        10 - Rey
+ * 
+ * Métodos:
+ * 
+ * darValor(): Recibe un número de palo y un valor y los asigna a la carta.
+ *             Si los datos son incorrectos, no realiza ninguna acción.
+ * 
+ * toString(): Devuelve un texto que describe la carta en formato de lectura
+ *             (por ejemplo: "As de Oros").
+ */
