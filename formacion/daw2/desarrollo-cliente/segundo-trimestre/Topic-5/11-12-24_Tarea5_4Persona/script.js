93feec9ac315aa7fc98f992d844a6aa2aa9b8c4f
Agu1406 <agustin6041@outlook.com>
2024-12-17 13:35:24 +0100

    feat(Objeto persona): Creación del objeto persona en Js.
    - Tiene anidación de objetos.
    - Tiene métodos internos.
    - Tiene atributos en forma de array.
    - Tiene console.log de sus métodos y valores.

--- formacion/daw2/desarrollo-cliente/segundo-trimestre/Topic-5/11-12-24_Tarea5_4Persona/script.js
+++ formacion/daw2/desarrollo-cliente/segundo-trimestre/Topic-5/11-12-24_Tarea5_4Persona/script.js
@@ -23,8 +23,62 @@ let persona = {
 
     // Creamos el atributo "edad" dentro de "persona"
     edad: 24,
+    // También creamos el atributo genero y un valor.
     genero : "masculino",
+    // También creamos (en un array) los intereses.
+    intereses : ["programación", "cocina"],
 
-    intereses : ["programación", "cocina"]
+    /**
+     * Podemos crear métodos / funciones dentro de los
+     * objetos, aquí haremos dos, uno llamado "saludo"
+     * que imprime en un "alert" el nombre de la persona.
+     * 
+     * Y otro que se llamara "bio" que tendra la edad
+     * y el genero (datos biologicos).
+     */
+    
+    // Función que imprime en un alert un saludo de persona.
+    saludo: function () {
+        alert("¡Hola! Mi nombre es: " + persona.nombre.nombre + " y mi apellido es: " + persona.nombre.apellido);
+    },
+    // Función que imprime los datos biologicos de la persona.
+    bio: function () {
+        alert(this.persona.nombre + " tiene " + persona.edad + " años y su genero es: " + persona.genero);
+    }
+};
 
-}
+/**
+ * Ahora que ya existen el objetos y sus atributos, podemos
+ * acceder a ellos y manipularlos.
+ */
+
+/**
+ * Dentro de "persona" existe un objeto llamado "nombre",
+ * en JavaScript se permite crear facilmente objetos dentro
+ * de objetos, se llama "anidación" de objetos, accedemos
+ * al objeto "persona", dentro del cual está el objeto
+ * "nombre" y dentro de nombre acceder a "nombre" o
+ * "apellido" según nos interese.
+ */
+console.log("Nombre: " + persona.nombre.nombre);
+console.log("Apellido: " + persona.nombre.apellido);
+
+/**
+ * Podemos acceder al array de intereses y usando posiciones
+ * podemos imprimir uno u otro.
+ */
+console.log(persona.intereses[0]);
+console.log(persona.intereses[1]);
+
+/**
+ * También podemos imprimir sus atributos que son a secas
+ * solo eso, atributos y valores, como la edad o genero.
+ */
+console.log(persona.genero);
+console.log(persona.edad);
+
+/**
+ * Probamos los métodos creados del objeto persona.
+ */
+persona.bio();
+persona.saludo();
