c7bdb98b03b1f51ee033cadcbe0e9483697f290d
Agu1406 <agustin6041@outlook.com>
2025-01-03 00:15:17 +0100

    feat(Ejercicio finalizado): Implementación completa del ejercicio.

--- formacion/daw1/programacion/src/GradoSuperiorDAW/SegundaEvaluacion/UT5EjerciciosConArrays/MPAAEjercicio3Contador.java
+++ formacion/daw1/programacion/src/GradoSuperiorDAW/SegundaEvaluacion/UT5EjerciciosConArrays/MPAAEjercicio3Contador.java
@@ -1,4 +1,21 @@
 package GradoSuperiorDAW.SegundaEvaluacion.UT5EjerciciosConArrays;
 
 public class MPAAEjercicio3Contador {
+    public static void main(String[] args) {
+        //Programa que cuenta cuantos pares hay en un array
+        int[] numeros = {1, 3, 4, 6, 7, 3, 2, 5, 8, 9, 8};
+
+        //Contador que contará cuantos pares hay. Empieza en cero
+        int cuentaPares = 0;
+
+        // Recorre uno por uno todos los números del array que hemos creado.
+        for(int numero : numeros) {
+            // Si el número en la posición actual del for es par el contador incrementa.
+            if(numero % 2 == 0){
+                cuentaPares = cuentaPares + 1;
+            }
+        }
+        // Fuera del bucle imprime cuantos pares hemos contado.
+        System.out.println("El numero total de pares es: " + cuentaPares);
+    }
 }
--- formacion/daw1/programacion/src/GradoSuperiorDAW/SegundaEvaluacion/UT5EjerciciosConArrays/MPAAEjercicio4Chivato.java
+++ formacion/daw1/programacion/src/GradoSuperiorDAW/SegundaEvaluacion/UT5EjerciciosConArrays/MPAAEjercicio4Chivato.java
@@ -1,4 +1,27 @@
 package GradoSuperiorDAW.SegundaEvaluacion.UT5EjerciciosConArrays;
 
 public class MPAAEjercicio4Chivato {
+    public static void main(String[] args) {
+        //Programa que me avisa cuando ocurre algo.
+
+        /*
+        * Queremos saber si hay algún número par y mostrar el mensaje al
+        * terminar de recorrer el array
+        * */
+        int[] numeros = {1, 3, 4, 6, 7, 3, 2, 5, 8, 9, 8};
+
+        /*
+        * Solo hay dos posibles resultados "true" o "false", luego usamos un boolean.
+        * Suponemos que no hay ninguno
+        * */
+        boolean hayPares = false;
+        // Bucle que recorre uno por uno todos los números del array.
+        for(int numero:numeros) {
+            // Si algún número es par convierte el "false" a "true".
+            if(numero % 2 == 0){
+                hayPares = true;
+            }
+        }
+        System.out.println("La respuesta a si había algún numero par es : " + hayPares);
+    }
 }
