d5cbc641555696042c1fd15a1575c5186457271a
Agu1406 <agustin6041@outlook.com>
2025-01-03 00:08:51 +0100

    feat(Ejercicio 2 del UT5): Creación de un acumulador de operaciones matematicas.

--- formacion/daw1/programacion/src/GradoSuperiorDAW/SegundaEvaluacion/UT5EjerciciosConArrays/MPAAEjercicio1IntercambioValores.java
+++ formacion/daw1/programacion/src/GradoSuperiorDAW/SegundaEvaluacion/UT5EjerciciosConArrays/MPAAEjercicio1IntercambioValores.java
@@ -1,4 +1,16 @@
 package GradoSuperiorDAW.SegundaEvaluacion.UT5EjerciciosConArrays;
 
-public class MPAAEjercicio1 {
+public class MPAAEjercicio1IntercambioValores {
+    public static void main(String[] args) {
+        int[] numeros = {1, 3, 4, 6, 7, 3, 2, 5, 8, 9, 8};
+        // Queremos intercambiar las posiciones 2 y 7 del array.
+        int numeroAuxiliar; // Usamos una variable auxiliar para hacer el intercambio
+        numeroAuxiliar = numeros[2];
+        numeros[2] = numeros[7];
+        numeros[7] = numeroAuxiliar;
+        // Hemos intercambiado los valores y los mostramos todos para visualizarlo
+        for (int numero : numeros) {
+            System.out.println(numero);
+        }
+    }
 }
--- formacion/daw1/programacion/src/GradoSuperiorDAW/SegundaEvaluacion/UT5EjerciciosConArrays/MPAAEjercicio2Acumulador.java
+++ formacion/daw1/programacion/src/GradoSuperiorDAW/SegundaEvaluacion/UT5EjerciciosConArrays/MPAAEjercicio2Acumulador.java
@@ -1,4 +1,25 @@
 package GradoSuperiorDAW.SegundaEvaluacion.UT5EjerciciosConArrays;
 
 public class MPAAEjercicio2Acumulador {
+    public static void main(String[] args) {
+        //Queremos sumar y multiplicar los valores de un array
+        int[] numeros = {1, 3, 4, 6, 7, 3, 2, 5, 8, 9, 8};
+
+        // Creamos una variable que guarda las sumas parciales hasta ese momento. Empieza en cero.
+        int sumaAcumulada = 0;
+
+        /*
+        * Lleva las multiplicaciones acumuladas hasta ese momento.
+        * Empieza en uno, porque si empezara en cero, todas serían cero.
+        * */
+        int productoAcumulado=1;
+        // Con un bucle recorremos uno por uno los números dentro del array "números".
+        for(int numero : numeros) {
+            sumaAcumulada = sumaAcumulada + numero;
+            productoAcumulado = productoAcumulado * numero;
+        }
+        // Imprimimos por consola los resultados del programa.
+        System.out.println("La suma total es: " + sumaAcumulada);
+        System.out.println("La multiplicacion total es: " + productoAcumulado);
+    }
 }
