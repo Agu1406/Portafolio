52729ecad3475fd73ec1e324185b1dedea388f05
Agu1406 <agustin6041@outlook.com>
2025-01-14 12:52:31 +0100

    feat(ej2spring): Utilización de lombok para getters y setter.
    - Remplace los Getters y Setter por "@Data" de lombok.
    - Documente todo el código con JavaDoc.
    - He modificado los directorios de "src" del Spring.

--- formacion/daw1/programacion/src/GradoSuperiorDAW/SegundaEvaluacion/UT5EjerciciosConArrays/MPAAEjercicio7Calificaciones.java
+++ formacion/daw1/programacion/src/GradoSuperiorDAW/SegundaEvaluacion/UT5EjerciciosConArrays/MPAAEjercicio7Calificaciones.java
@@ -1,4 +1,66 @@
 package GradoSuperiorDAW.SegundaEvaluacion.UT5EjerciciosConArrays;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class MPAAEjercicio7Calificaciones {
+    public static void main(String[] args) {
+        // Instancia de la clase Scanner para pedir números por consola
+        Scanner teclado = new Scanner(System.in);
+        // Arraylist que almacenara las 5 calificaciones
+        ArrayList<Integer> calificaciones = new ArrayList<>();
+        // Variable que cambiara de valor cada vez que se solicite una calificación.
+        int calificacion;
+        // Variables para guardar la maxima nota, la minima y el promedio.
+        int max, min, promedio = 0;
+
+        // Aviso de lo que se va a solicitar.
+        System.out.println("¡Por favor! Ingresa tús calificaciones.");
+
+        // Bucle que solicita 5 veces una calificación y la pushea al array.
+        for (int iteracion = 0; iteracion < 5; iteracion++) {
+            // Try-Catch que evita introducir algo que no sea un número.
+            try {
+                System.out.println("Ingresa una calificación. ");
+                // Intento pedir una calificación por teclado.
+                calificacion = teclado.nextInt();
+
+                // Pushea la calificación dentro del array.
+                calificaciones.add(calificacion);
+            } catch (InputMismatchException e) {
+                System.out.println("¡Error! Solo puedes ingresar números enteros.");
+                // Limpia el buffer del teclado.
+                teclado.next();
+                // Decrementa la iteración para repetirla.
+                iteracion--;
+            }
+        }
+
+        // En "max" y "min" guardamos la primera posición del array para ir comparando
+        max = calificaciones.get(0);
+        min = calificaciones.get(0);
+
+        // Bucle que reccore todas las posibles posiciones del array.
+        for (int posicion = 0; posicion < calificaciones.size(); posicion++) {
+            // Si el valor en "posición" es mayor que "max" actualiza el valor.
+            if (calificaciones.get(posicion) > max) {
+                max = calificaciones.get(posicion);
+            }
+            // Si el valor en "posición" es menor que "min" actualiza el valor.
+            if (calificaciones.get(posicion) < min) {
+                min = calificaciones.get(posicion);
+            }
+            // Suma una por una todas las calificaciones para luego calcular el promedio.
+            promedio += calificaciones.get(posicion);
+        }
+
+        // Al final del bucle, calculamos el promedio.
+        promedio = promedio / calificaciones.size();
+
+        // Imprimimos por consola.
+        System.out.println("Nota más alta: " + max);
+        System.out.println("Nota más baja: " + min);
+        System.out.println("Promedio de notas: " + promedio);
+    }
 }
---
+++ formacion/daw2/desarrollo-servidor/segundo-trimestre/U7/ej2spring/src/main/java/es/daw2/ej2spring/controladores/ControladorGrupos.java
@@ -0,0 +1,108 @@
+package es.daw2.ej2spring.controladores;
+
+import org.springframework.web.bind.annotation.*;
+import es.daw2.ej2spring.modelos.Grupo;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Controlador REST para gestionar los grupos de formación.
+ * Este controlador permite realizar operaciones CRUD sobre
+ * un conjunto de grupos almacenados en un ArrayList que
+ * simula una "base de datos".
+ *
+ */
+@RestController
+public class ControladorGrupos {
+    /*
+     * Como estámos trabajando sin una base de datos (por el momento)
+     * para poder aprender, creamos un ArrayList de instancias de la
+     * clase Grupo para poder cargar esos datos manualmente y
+     * usarlos como "base de datos".
+     */
+    ArrayList<Grupo> grupos = new ArrayList<>(
+            // El ArrayList es una "lista" de instancias de Grupo.
+            List.of(
+                    new Grupo("Ventura", "DAW", "1"),
+                    new Grupo("Ventura", "DAW", "2"),
+                    new Grupo("Ventura", "ASIR", "1"),
+                    new Grupo("Ventura", "ASIR", "2"),
+                    new Grupo("Ventura", "SMR", "1"),
+                    new Grupo("Ventura", "SMR", "2"),
+                    new Grupo("Los Álamos", "DAW", "1"),
+                    new Grupo("Los Álamos", "DAW", "2"),
+                    new Grupo("Zayas", "DAW", "1"),
+                    new Grupo("Zayas", "DAW", "2")));
+
+    /**
+     * Crea un nuevo grupo y lo agrega a la lista de grupos.
+     * @param nuevoGrupo Objeto Grupo recibido desde la URL.
+     * @return Lista actualizada de grupos.
+     */
+    @PostMapping("/grupos/crear")
+    public ArrayList<Grupo> crearGrupo(@PathVariable Grupo nuevoGrupo) {
+        // Ya recibido el "grupo" desde la URL, lo añadimos al ArrayList
+        grupos.add(nuevoGrupo);
+        // Devolvemos la lista de grupos actualizada.
+        return grupos;
+    }
+
+    /**
+     * Crea y lista los grupos de un instituto (IES) específico.
+     * @param ies Nombre del instituto recibido desde la URL.
+     * @return Lista de grupos pertenecientes al instituto.
+     */
+    @GetMapping("/grupos/crear/{ies}")
+    public ArrayList<Grupo> gruposDeUnIes(@PathVariable String ies) {
+        // Crea un nuevo ArrayList de grupos llamado "nuevoGrupo"
+        ArrayList<Grupo> nuevogrupo = new ArrayList<Grupo>();
+        for (Grupo grupo : grupos) {
+            if (grupo.getIes().equalsIgnoreCase(ies)) {
+                nuevogrupo.add(grupo);
+            }
+        }
+        return nuevogrupo;
+    }
+
+    /**
+     * Método READ (CRUD) que permita listar todos los grupos existentes
+     * en la lista de grupos creados.
+     *
+     * @return la lista de grupos actualizada.
+     */
+    @GetMapping("/grupos/listar")
+    public ArrayList<Grupo> listarGrupos() {
+        // Devuelve la lista de grupos en su versión más reciente.
+        return grupos;
+    }
+
+    /**
+     * Actualiza el instituto (IES) de un grupo existente.
+     * @param ies Nuevo nombre del instituto recibido desde la URL.
+     * @return Lista actualizada de grupos.
+     */
+    @PutMapping("/grupos/update/")
+    public ArrayList<Grupo> actualizarGrupo(@PathVariable String ies) {
+        return grupos;
+    }
+
+    /**
+     * Elimina un grupo con base en el instituto (IES) proporcionado.
+     * @param ies Nombre del instituto recibido desde la URL.
+     * @return Lista actualizada de grupos.
+     */
+    @DeleteMapping("/grupos/borrar/")
+    public ArrayList<Grupo> borrarGrupo(@PathVariable String ies) {
+        // Recorre con un foreach todos los grupos del Arraylist.
+        for (Grupo grupo : grupos) {
+            // Si consigue cualquier grupo con el mismo "ies" ejecuta el IF.
+            if (grupo.getIes().equalsIgnoreCase(ies)) {
+                // Remueve del ArrayList el grupo cuyo "ies" concuerde.
+                grupos.remove(grupo);
+            }
+        }
+        // Devuelve la lista actualizada de grupos.
+        return grupos;
+    }
+
+}
