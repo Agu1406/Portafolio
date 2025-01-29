977dfc6157e64421e586a5d76d43b05cdd011ffd
Agu1406 <agustin6041@outlook.com>
2025-01-29 08:46:31 +0100

    feat(Tarea6_2Marcado): Busqueda dinamica de todas las apariciones de "X" palabra con el marcado de la misma en sus posiciones con Ts o Js y CSS/HTML.

---
+++ formacion/daw2/desarrollo-cliente/segundo-trimestre/Topic-6/01-28-25-T6_4Marcado/script.js
@@ -0,0 +1,4 @@
+/**
+ * No usar la variable "var", está en deshuso y no es profesional, usar
+ * let o const en su lugar.
+ */
--- formacion/daw2/desarrollo-cliente/segundo-trimestre/Topic-6/formato-estandar-tareas/script.js
+++ formacion/daw2/desarrollo-cliente/segundo-trimestre/Topic-6/formato-estandar-tareas/script.js
@@ -2,3 +2,17 @@
  * No usar la variable "var", está en deshuso y no es profesional, usar
  * let o const en su lugar.
  */
+
+/**
+ * Crea una página con una gran cantidad de texto.
+ *
+ * Solicita al usuario una palabra a buscar.
+ * Realiza scroll hasta la primera ocurrencia de esa palabra (la palabra aparecerá remarcada).
+ * Consejo: Aunque no es estándar, puede ser útil el método find() de la interfaz Window de JS.
+ *
+ * Cuando lo consigas, haz que:
+ * A los tres segundos de encontrarla, salta un aviso indicando que se calculará el número de veces que aparece el texto en la página.
+ * Cuando el usuario acepte, la página mostrará el número de ocurrencias del texto.
+ * Se muestra el texto buscado y su cantidad de ocurrencias al principio de la página (y se traslada allí).
+ */
+
