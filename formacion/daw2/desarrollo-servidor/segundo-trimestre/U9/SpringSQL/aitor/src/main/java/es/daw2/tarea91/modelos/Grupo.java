aa6436d2fe413c4c6dda15674d85b3cd11341c0b
Agu1406 <agustin6041@outlook.com>
2025-01-29 09:50:00 +0100

    feat(Tarea91): Correción de conexión a la BD.

--- formacion/daw2/desarrollo-servidor/segundo-trimestre/U9/SpringSQL/aitor/src/main/java/es/daw2/tarea91/modelos/Grupo.java
+++ formacion/daw2/desarrollo-servidor/segundo-trimestre/U9/SpringSQL/aitor/src/main/java/es/daw2/tarea91/modelos/Grupo.java
@@ -1,31 +1,48 @@
 package es.daw2.tarea91.modelos;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-// Anotaciones de lombok
+// LOMBOK: Generar al compilar "Getters" y "Setters".
 @Data
+// LOMBOK: Generar dal compilar un constructor sin argumentos.
 @NoArgsConstructor
+// LOMBOK: Generar al compilar un constructor con todos los argumentos.
 @AllArgsConstructor
-// Anotaciones de jakarta persistente
+// JPA: Especifica que la clasa es una entidad en una base de datos.
 @Entity
-@Table (name = "GRUPOS")
+// JPA: Especifica la tabla principal de la entidad.
+@Table(name = "GRUPOS")
 public class Grupo {
-    @Id @GeneratedValue
-    private Long id;
+    // "Id" especifica la clave primeraria de la tabla.
+    @Id
+    // Permite que la clave se genera automaticamente de forma incrementativa.
+    @GeneratedValue
+    private Long idGrupo;
 
-    @Column (name = "IES", nullable = false, columnDefinition = "varchar(25)")
+    // Define una columna de la tabla, si su valor puede ser null y el tipo de dato.
+    @Column(name = "IES", nullable = false, columnDefinition = "varchar(25)")
     private String ies;
-
-    @Column (name = "CICLO", nullable = false, columnDefinition = "varchar(125)")
+    
+    // Define una columna de la tabla, si su valor puede ser null y el tipo de dato.
+    @Column(name = "CICLO", nullable = false, columnDefinition = "varchar(125)")
     private String ciclo;
-
-    @Column (name = "CURSO", nullable = false, columnDefinition = "integer default 1")
+    
+    // Define una columna de la tabla, si su valor puede ser null y el tipo de dato.
+    @Column(name = "CURSO", nullable = false, columnDefinition = "integer default 1")
     private String curso;
+
+    // Un grupo puede tener uno o varios alumnos (1:N)
+    @OneToMany(mappedBy = "grupo")
+    // Lista de alumnos pertenecientes a "X" grupo.
+    private List<Alumno> alumnos;
 }
