2ab621d0dff81d2dc57ccd9c980cc2f4306cfaf2
Agu1406 <agustin6041@outlook.com>
2025-02-06 15:54:00 +0100

    chorus(Corrección): Modificación del ".bat" que genera automaticamente archivos HTML con un nombre personalizado para que estos ahora tenga un cuerpo personalizado también.

--- formacion/daw2/interfaces-web/Segundo Trimestre/12-09-24_Ejercicios140-150/ejecutable.bat
+++ formacion/daw2/interfaces-web/Segundo Trimestre/12-09-24_Ejercicios140-150/ejecutable.bat
@@ -14,11 +14,35 @@ set "prefijo=MPAA_Ejercicio"
 set "extension=.html"
 
 REM Definimos el contenido que se escribirá en cada archivo HTML
-set "contenido=<!DOCTYPE html>\n<html lang='es'>\n<head>\n    <meta charset='UTF-8'>\n    <meta name='viewport' content='width=device-width, initial-scale=1.0'>\n    <title>Nombre de la actividad</title>\n    <link rel='icon' type='image/x-icon' href='https://iesventura.es/_ies_datos/LOGO_new.jpg'>\n    <link rel='stylesheet' href='estilos.css'>\n</head>\n<body>\n\n</body>\n</html>"
+set "contenido=<!DOCTYPE html>^
+<html lang='es'>^
+<head>^
+    <meta charset='UTF-8'>^
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>^
+    <title>Nombre de la actividad</title>^
+    <link rel='icon' type='image/x-icon' href='https://iesventura.es/_ies_datos/LOGO_new.jpg'>^
+    <link rel='stylesheet' href='estilos.css'>^
+</head>^
+<body>^
+    <div class='contenedor'>^
+        <header>^
+            <p>Desarrollo Web Entorno Cliente.</p>^
+        </header>^
+        <main>^
+            <h1>Titulo de la actividad</h1>^
+            <p>Descripción de la actividad.</p>^
+        </main>^
+        <footer>^
+            <p>&copy; 2024 <a href="https://github.com/agu1406" target="_blank">Github Agu1406</a>. Todos los derechos reservados.</p>^
+        </footer>^
+    </div>^
+    <script src='script.js'></script>^
+</body>^
+</html>"
 
 REM Usamos un bucle "for" que cree un documento con prefijo y extensión y escribe contenido.
 for /L %%i in (!inicio!,!incremento!,!final!) do (
-    echo.!contenido! > "!prefijo!%%i!extension!"
+    echo !contenido! > "!prefijo!%%i!extension!"
 )
 
 echo Archivos creados correctamente.
