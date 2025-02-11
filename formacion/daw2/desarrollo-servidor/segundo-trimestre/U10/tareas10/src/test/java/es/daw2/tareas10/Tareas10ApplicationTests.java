02b5e03d5ba879eb0eb807c1367e9c01d9ecb53c
Agu1406 <agustin6041@outlook.com>
2025-02-11 08:50:48 +0100

    feat(tareas10): Creación de un proyecto Spring Boot con las siguientes dependencias:
    - Spring Web
    - Lombok
    - Thymeleaf

---
+++ formacion/daw2/desarrollo-servidor/segundo-trimestre/U10/tareas10/src/test/java/es/daw2/tareas10/Tareas10ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.daw2.tareas10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tareas10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
