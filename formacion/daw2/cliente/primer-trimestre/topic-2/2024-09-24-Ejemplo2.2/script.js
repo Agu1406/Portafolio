b5e884edb480d08816082e0515eff23b1952004f
Agu1406 <agustin6041@outlook.com>
2025-03-22 23:25:45 +0100

    feat(Ejemplo2.2): Explicación de tipos de variables en Js (var, let, const) y sus usos y alcance.

---
+++ formacion/daw2/cliente/primer-trimestre/topic-2/2024-09-24-Ejemplo2.2/script.js
@@ -0,0 +1,36 @@
+const A = 6,
+    B = 2;
+chooseOperator();
+operate(operator); // FALLARÁ
+
+
+function chooseOperator() {
+    console.log('Eligiendo operador...');
+    let operator = prompt('Indica la operación deseada escribiendo + , -, * o /', '+');
+    console.log('La operación es ' + operator);
+}
+
+
+function operate(operator) {
+    console.log('Operando...');
+    switch (operator) {
+        case '+':
+            result = A + B;
+            console.log('La suma es: ' + result);
+            break;
+        case '-':
+            result = A - B;
+            console.log('La diferencia es: ' + result);
+            break;
+        case '*':
+            result = A * B;
+            console.log('El producto es: ' + result);
+            break;
+        case '/':
+            result = A / B;
+            console.log('El cociente es: ' + result);
+            break;
+        default:
+            chooseOperator();
+    }
+}
