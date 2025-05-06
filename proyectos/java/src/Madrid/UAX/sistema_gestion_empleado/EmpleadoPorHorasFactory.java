19ed843320e603d07289ef9e9f0dd3affc6dd271
Agu1406 <agustin6041@outlook.com>
2025-05-06 17:26:12 +0200

    feat(sistema_gestion_empleado): Implementación de clases para la gestión de empleados, incluyendo interfaces y clases para empleados asalariados y por horas. Se añaden fábricas para la creación de empleados y se implementa la persistencia de datos.

---
+++ proyectos/java/src/Madrid/UAX/sistema_gestion_empleado/EmpleadoPorHorasFactory.java
@@ -0,0 +1,13 @@
+package Madrid.UAX.sistema_gestion_empleado;
+
+public class EmpleadoPorHorasFactory implements EmpleadoFactory {
+    @Override
+    public Empleado crearEmpleado(String nombre, double salarioBase, Object... params) {
+        if (params.length < 2) {
+            throw new IllegalArgumentException("Se requieren horas trabajadas y tarifa por hora");
+        }
+        int horasTrabajadas = (int) params[0];
+        double tarifaPorHora = (double) params[1];
+        return new EmpleadoPorHoras(nombre, salarioBase, horasTrabajadas, tarifaPorHora);
+    }
+} 
