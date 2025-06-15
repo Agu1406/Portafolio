b227ee0c12bfd2c53811efb7f7f839cd27bc7d5f
Agustin Antonio Márquez Piña <agustin6041@outlook.es>
2025-06-15 21:31:26 +0200

    chorus(eliminación de archivos): Se eliminan múltiples archivos de la estructura del proyecto, incluyendo clases y archivos relacionados con la gestión de cuentas bancarias, bibliotecas, empleados y ajedrez, así como archivos de configuración y de ejemplo. Esta limpieza busca optimizar el código y eliminar componentes no utilizados.

--- proyectos/java/src/Madrid/UAX/sistema_gestion_empleado/SalarioInvalidoException.java
+++
@@ -1,7 +0,0 @@
-package Madrid.UAX.sistema_gestion_empleado;
-
-public class SalarioInvalidoException extends RuntimeException {
-    public SalarioInvalidoException(String mensaje) {
-        super(mensaje);
-    }
-} 
