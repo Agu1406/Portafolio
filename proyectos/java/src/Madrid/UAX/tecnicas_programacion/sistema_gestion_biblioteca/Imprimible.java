75fee585c9eaebde38c8ed6f33342c226760307f
Agustin Antonio Márquez Piña <agustin6041@outlook.es>
2025-10-17 02:07:07 +0200

    chore(portafolio): Realiza una limpieza general de todos los archivos que se han movido al nuevo repositorio "ClasesParticulares" para preservar y guardar el contenido que imparto y aprendo de mis alumnos.

--- proyectos/java/src/Madrid/UAX/tecnicas_programacion/sistema_gestion_biblioteca/Imprimible.java
+++
@@ -1,7 +0,0 @@
-package Madrid.UAX.sistema_gestion_biblioteca;
-
-public interface Imprimible {
-
-    void imprimir();
-
-}
--- proyectos/java/src/Madrid/UAX/tecnicas_programacion/sistema_gestion_empleado/EmpleadoFactory.java
+++
@@ -1,5 +0,0 @@
-package Madrid.UAX.sistema_gestion_empleado;
-
-public interface EmpleadoFactory {
-    Empleado crearEmpleado(String nombre, double salarioBase, Object... params);
-} 
